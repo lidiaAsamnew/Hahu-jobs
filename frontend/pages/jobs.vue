d169c35f4dc2fbfa3c12ae340697d7279ea7fb2a
lidiaAsamnew <lidia.yirbe@gmail.com>
2025-08-12 15:10:46 +0300

    add jobs-page

---
+++ frontend/pages/jobs.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- 
+      This is the main content of your index.vue page. 
+      The Dialog will appear ON TOP of this page.
+    -->
+    <PagesIndex />
+
+    <!-- The Jobs by Sector Dialog Component -->
+    <JobsBySectorDialog :is-open="isJobsDialogOpen" @close="closeJobsDialog" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import PagesIndex from '~/pages/index.vue' // Import the homepage to show it in the background
+
+// Create a reactive variable to control the dialog's visibility
+const isJobsDialogOpen = ref(true) // Start with the dialog open
+
+// Function to close the dialog
+const closeJobsDialog = () => {
+  isJobsDialogOpen.value = false
+
+  useRouter().push('/')
+}
+</script>
